64bf17053d56650a0fcc773e33cfa2c92337d6fa
KradGm <enzo.loliveira@hotmail.com>
2023-06-12 14:33:13 -0300

    code organization

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { WeatherApiService } from '../weather-api.service';
 import { IonSearchbar } from '@ionic/angular';
 
@@ -12,33 +12,29 @@ import { IonSearchbar } from '@ionic/angular';
 export class HomePage implements OnInit {
   @ViewChild('mySearchBar') searchBar!: IonSearchbar;
   temperature!: number;
-  name: string;
+  locationName: string;
   searchTerm: string | null | undefined;
   resultados: any[] = [];
-  constructor(private weather: WeatherApiService) {this.searchTerm = '';this.name = '';}
-  ngOnInit(){
-    //this.weather.getWeather().subscribe(data => {
-     // this.temperature = data.current.temp_c;
-      //this.name = data.location.name;
-  //});
-}
-  HandleInput(){
-  this.searchTerm = this.searchBar.value;
-  this.weather.q = this.searchTerm;
-  if(this.searchTerm?.length! >= 3 && this.searchTerm?.length! <= 15){
-    this.weather.autoComplete().subscribe(data => {this.name = data.name; this.resultados = data; console.log(data);});
+  constructor(private weather: WeatherApiService) { this.searchTerm = ''; this.locationName = ''; }
+  ngOnInit() { }
+
+  HandleInput() {
+    this.searchTerm = this.searchBar.value;
+    this.weather.q = this.searchTerm;
+    if (this.searchTerm?.length! >= 3 && this.searchTerm?.length! <= 15) {
+      this.weather.autoComplete().subscribe(data => { this.locationName = data.name; this.resultados = data; console.log(data); });
+    }
+    else {
+      this.resultados = [];
+    }
   }
-  else {
-    this.resultados = [];
-  }
-  }
-  PopulateCard(resultadoCity: string | null | undefined){ 
-  console.log(resultadoCity);
-  resultadoCity = this.weather.q;
-  this.weather.getWeather().subscribe(data => {
-    this.temperature = data.current.temp_c;
-    this.name= data.location.name;
-  })
+  PopulateCard(resultadoCity: string | null | undefined) {
+    console.log(resultadoCity);
+    resultadoCity = this.weather.q;
+    this.weather.getWeather().subscribe(data => {
+      this.temperature = data.current.temp_c;
+      this.locationName = data.location.name;
+    })
   }
 }
 
